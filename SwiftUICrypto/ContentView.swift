1329b2a489ec4f802edfccbb9462fec9bd22e380
medhatmebed <medhat.mebed@gmail.com>
2023-12-30 04:32:22 +0200

    Initial Commit

---
+++ SwiftUICrypto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUICrypto
+//
+//  Created by Medhat Mebed on 12/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUICrypto/SwiftUICryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICryptoApp.swift
+//  SwiftUICrypto
+//
+//  Created by Medhat Mebed on 12/30/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
